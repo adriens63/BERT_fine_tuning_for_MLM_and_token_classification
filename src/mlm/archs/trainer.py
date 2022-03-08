d15cd7cbfc9b82ac86ba0a0f228762f0af4d61d3
adriens63 <serviere.adrienn@gmail.com>
2022-03-08 09:25:22 +0000

    add new features

--- src/mlm/archs/trainer.py
+++ src/mlm/archs/trainer.py
@@ -1,16 +1,6 @@
 import torch
-from torchsummary import summary
-from torch.utils.tensorboard import SummaryWriter
-import torch.distributed as dist
-import numpy as np
 from tqdm import tqdm
-import os
-import os.path as osp
-from datasets import load_metric
-import json
 
-from src.tools.model_summary import summary_parameters
-from src.tools.timer import timeit
 from src.tools.base_trainer import BaseTrainer
 
 
@@ -37,54 +27,49 @@ class Trainer(BaseTrainer):
             val_data_loader,
             val_steps,
             checkpoint_frequency,
+            metric_frequency,
+            model_name,
+            weights_path
+    ) -> None:
+
+        super().__init__(
+            device,
+            model,
+            epochs,
+            batch_size,
+            loss_fn,
+            optimizer,
+            lr_scheduler,
+            patience,
+            train_data_loader,
+            train_steps,
+            val_data_loader,
+            val_steps,
+            checkpoint_frequency,
+            metric_frequency,
             model_name,
             weights_path,
-            ) -> None:
-        
-        self.device = device
-        self.model = torch.nn.DataParallel(model, device_ids = [i for i in range(torch.cuda.device_count())]).to(device)
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.loss_fn = loss_fn
-        self.optimizer = optimizer
-        self.lr_scheduler = lr_scheduler
-        self.patience = patience
-        self.train_data_loader = train_data_loader
-        self.train_steps = train_steps
-        self.val_data_loader = val_data_loader
-        self.val_steps = val_steps
-        self.checkpoint_frequency = checkpoint_frequency
-        self.model_name = model_name
-        self.weights_path = weights_path
-        self.mod_dir = weights_path + model_name + '/'
-        self.log_dir = weights_path + model_name + '/log_dir/'
-        self.ckp_dir = weights_path + model_name + '/ckp_dir/'
-
-        self.metric = load_metric('accuracy')
-        self.tmp_metric = load_metric('accuracy')
-        self.loss = {"train": [], "val": []}
-        self.acc = {"train": [], "val": []}
-        self.w = SummaryWriter(log_dir = self.log_dir)
-        self.last_loss = np.inf
-        self.trigger_times = 0
+        )
 
 
 
     def train(self) -> None:
 
         print('.... MLM CamemBERT training:')
+        print(self.metric_frequency, self.train_metric_steps, self.val_metric_steps)
         super().train()
         print('training done;')
         print()
-        
+
 
 
     def _train_step(self) -> None:
-        
+
         self.model.train()
 
         loop = tqdm(self.train_data_loader)
-        running_loss = 0  
+        running_loss = 0
+        running_acc = 0
         n_batches = 0
 
         for i, batch in enumerate(loop):
@@ -95,23 +80,37 @@ class Trainer(BaseTrainer):
             input_ids = batch['input_ids'].to(self.device)
             attention_mask = batch['attention_mask'].to(self.device)
             labels = batch['labels'].to(self.device)
-            
-            loss, out = self.model(input_ids, attention_mask = attention_mask, labels = labels).to_tuple()
+
+            loss, out = self.model(
+                input_ids, attention_mask=attention_mask, labels=labels).to_tuple()
 
             loss.sum().backward()
             self.optimizer.step()
 
-            running_loss += loss.sum().item()
-            predictions = torch.argmax(out, dim = -1)
-            self.metric.add_batch(predictions = predictions.view(-1), references = batch["labels"].view(-1))
-            self.tmp_metric.add_batch(predictions = predictions.view(-1), references = batch["labels"].view(-1))
-
-            loop.set_postfix(loss = loss.sum().item(), acc = self.tmp_metric.compute()['accuracy'])
             
+            predictions = torch.argmax(out, dim=-1)
+            self.metric.add_batch(
+                predictions=predictions.view(-1), references=batch["labels"].view(-1))
+            self.tmp_metric.add_batch(
+                predictions=predictions.view(-1), references=batch["labels"].view(-1))
+
+            epoch_loss = loss.sum().item()
+            epoch_acc = self.tmp_metric.compute()['accuracy']
+            running_loss += epoch_loss
+            running_acc += epoch_acc
+
+            loop.set_postfix(loss=epoch_loss,
+                             acc=epoch_acc)
+
+            if i % self.train_metric_steps == 0:
+
+                self.epoch_loss['train'].append(running_loss / n_batches)
+                self.epoch_acc['train'].append(running_acc / n_batches)
+
             if i == self.train_steps:
 
                 break
-        
+
         self.loss['train'].append(running_loss / n_batches)
         acc = self.metric.compute()['accuracy']
         self.acc['train'].append(acc)
@@ -136,21 +135,32 @@ class Trainer(BaseTrainer):
 
             with torch.no_grad():
 
-                loss, out = self.model(input_ids, attention_mask = attention_mask, labels = labels).to_tuple()
+                loss, out = self.model(
+                    input_ids, attention_mask=attention_mask, labels=labels).to_tuple()
 
-            running_loss += loss.sum().item()
+            predictions = torch.argmax(out, dim=-1)
+            self.metric.add_batch(
+                predictions=predictions.view(-1), references=batch["labels"].view(-1))
+            self.tmp_metric.add_batch(
+                predictions=predictions.view(-1), references=batch["labels"].view(-1))
 
-            predictions = torch.argmax(out, dim = -1)
-            self.metric.add_batch(predictions = predictions.view(-1), references = batch["labels"].view(-1))
-            self.tmp_metric.add_batch(predictions = predictions.view(-1), references = batch["labels"].view(-1))
+            epoch_acc = self.tmp_metric.compute()['accuracy']
+            epoch_loss = loss.sum().item()
 
-            loop.set_postfix(loss = loss.sum().item(), acc = self.tmp_metric.compute()['accuracy'])
+            running_loss += epoch_loss
+
+            loop.set_postfix(loss=epoch_loss, acc=epoch_acc)
+
+            if i % self.val_metric_steps == 0:
+
+                self.epoch_loss['val'].append(running_loss / n_batches)
+                self.epoch_acc['val'].append(epoch_acc)
 
             if i == self.val_steps:
 
-                    break
-            
-        self.loss["val"].append(running_loss / n_batches)
+                break
+        
+        self.loss['val'].append(running_loss / n_batches)
         acc = self.metric.compute()['accuracy']
-        self.acc["val"].append(acc)
+        self.acc['val'].append(acc)
 
